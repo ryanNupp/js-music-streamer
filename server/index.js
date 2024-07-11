24d0efabea32b8fc579f1e6e12bcb0e0da8829c7
ryanNupp <ryan.nuppenau@icloud.com>
2024-07-11 17:08:31 -0500

    Temp music folder & backend get request for streaming music files

--- server/index.js
+++ server/index.js
@@ -1,25 +1,42 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+// const multer = require('multer');  // this will be used for 
+const fs = requrie('fs');
+const app = express();
 
-app.use(cors())
+const port = 8080; // port variable -- TODO: let user choose custom port
+const folder = 'temp_music_collection';
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello from our server!')
+})
+
+// routes for streaming
+app.get('/stream/:filename', (req, res) => {
+    const { filename } = req.params;
+    const file = `${folder}/${filename}`;
+    if (fs.existsSync(file)) {
+        const audioStream = fs.createReadStream(file);
+        audioStream.pipe(res); // Stream audio file to the client
+    } else {
+        res.status(404).send('File not found');
+    }
 })
 
 //making gets for pages Albums, Artists and playlists?
 app.get('/albums', (req, res) => {
     res.send('This is the Albums Page')
 })
-
 app.get('/artists', (req, res) => {
     res.send('This is the Artist Page')
 })
-
 app.get('/playlists', (req, res) => {
     res.send('This is the playlist Page')
 })
-app.listen(8080, () => {
-      console.log('server listening on port 8080')
+
+// Start server
+app.listen(port, () => {
+      console.log(`server listening on port ${port}`)
 }) 
